a1c61fe20296181e834cab04b2ae8fbe1886d7a6
Luna Jimenez <pjjf97@gmail.com>
2019-10-24 10:55:32 +0200

    Procesados numeros

--- analex.py
+++ analex.py
@@ -56,8 +56,7 @@ class Analex:
   else:
    self.flujo.devuelve(ch)
   if real:
-   v = float(l)  # if ch:
-   # v = int(l[:-1])
+   v = float(l)
   else:
    v = int(l)
   return v
@@ -137,7 +136,14 @@ class Analex:
   elif ch== "\n":
    ## acciones al encontrar un salto de linea
    self.nlinea = self.nlinea + 1
-   return self.Analiza()
+   return componentes.nl()
+  elif ch.isdecimal():
+   resultado = self.TrataNum(self.flujo, ch)
+   if isinstance(resultado, int):
+     return componentes.Numero(self.nlinea, resultado, "int")
+   else:
+     return componentes.Numero(self.nlinea, resultado, "real")
+
   elif ch:
     # se ha encontrado un caracter no permitido
     print ("ERROR LEXICO  Linea "+  str(self.nlinea) +" ::  Caracter "+ ch +" invalido ")
