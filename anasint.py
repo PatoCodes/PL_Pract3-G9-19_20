c1758f908bfb2269342be521730208a6c621cce6
Machimari* <machimari@avalon.es>
2019-11-28 11:00:51 +0100

    SÍ

--- anasint.py
+++ anasint.py
@@ -13,33 +13,34 @@ import errores
 class Sintactico:
 #Constructor de la clase que implementa el Analizador Sintactico
 #Solicita el primer compnente lexico 
- def __init__(self, lexico):
+  def __init__(self, lexico):
     self.lexico= lexico
     self.token=self.lexico.Analiza()
 
 # Funcion que muestra los mensajes de error
- def Error(self, nerr, tok):
-     if nerr == 1:
-         print ("Linea: " + str(self.token.linea) + "  ERROR Se espera PROGRAMA")
+  def Error(self, nerr, tok):
+    if nerr == 1:
+      print ("Linea: " + str(self.token.linea) + "  ERROR Se espera PROGRAMA")
  
 # TERMINALES
- def Programa(self):
-  pass
+  def Programa(self):
+    pass
+
 
 ########################################################
 ##
 ## PRograma principal que lanza el analizador sintactico
 ####################################################
 if __name__=="__main__":
-    script, filename=argv
-    txt=open(filename)
-    print ("Este es tu fichero %r" % filename)
-    i=0
-    fl = flujo.Flujo(txt)
-    anlex=analex.Analex(fl)
-    S = Sintactico(anlex)
-    if S.Programa():
-      print ("Analisis sintactico SATISFACTORIO. Fichero :", filename, "CORRECTO")
-    else:
-      print ("Analisis sintactico CON ERRORES. Fichero :", filename, "ERRONEO")
+  script, filename=argv
+  txt=open(filename)
+  print ("Este es tu fichero %r" % filename)
+  i=0
+  fl = flujo.Flujo(txt)
+  anlex=analex.Analex(fl)
+  S = Sintactico(anlex)
+  if S.Programa():
+    print ("Analisis sintactico SATISFACTORIO. Fichero :", filename, "CORRECTO")
+  else:
+    print ("Analisis sintactico CON ERRORES. Fichero :", filename, "ERRONEO")
 
